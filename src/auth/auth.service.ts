dfa32fb6f3e8c55451bd5bb2edcce26bba13819e
nikitsch <nikitsch5@gmail.com>
2023-05-09 22:25:06 +0300

    refactor: prettier code

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -21,7 +21,7 @@ export class AuthService {
     if (candidate) {
       throw new HttpException('Пользователь с таким email уже существует', HttpStatus.BAD_REQUEST)
     }
-    const hashPassword = await bcrypt.hash(userDto.password, 5);   
+    const hashPassword = await bcrypt.hash(userDto.password, 5);
     const user = await this.userService.createUser({ ...userDto, password: hashPassword })
     return this.generateToken(user)
   }
