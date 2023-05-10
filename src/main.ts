4e41b9f6990d76447bf641a0603ae04a55101085
nikitsch <nikitsch5@gmail.com>
2023-05-10 19:55:56 +0300

    feat: add pipes for validation and use glodal Pipes

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from "@nestjs/core";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
+import { ValidationPipe } from "./pipes/validation.pipe";
 
 
 async function start() {
@@ -17,7 +18,7 @@ async function start() {
   const document = SwaggerModule.createDocument(app, config)
   SwaggerModule.setup('/api/docs', app, document)
 
-  // app.useGlobalPipes()
+  app.useGlobalPipes(new ValidationPipe)
 
   await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`))
 
